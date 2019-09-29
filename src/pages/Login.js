be82d7680b8ef5e949b2384cb4635a38cb74f5f6
Guilherme Girardi <girardi.gui@icloud.com>
2019-09-29 18:51:16 +0100

    last update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,20 +9,15 @@ export default function Login({ history }) {
     history: PropTypes.any,
   };
 
-  // State.
-  // When I need to edit the user name I call "setUsername", when I need the user name, I call "username"
   const [username, setUsername] = useState('');
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    // calling the api (axios), so use "async/await" to get all the information about the user.
     const response = await api.post('/devs', {
       username,
     });
 
-    // What do I want? Only the id of the user, so...
-    // _id is inside of 'response.data'
     const { _id } = response.data;
 
     history.push(`/dev/${_id}`);
@@ -32,7 +27,6 @@ export default function Login({ history }) {
     <div className="login-container">
       <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
-        {/* onChange = Event will set on 'setUsername' the value of the target which is the input. */}
         <input placeholder="Type your GitHub user" value={username} onChange={e => setUsername(e.target.value)} />
         <button type="submit">Send</button>
       </form>
